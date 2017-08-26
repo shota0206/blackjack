addf5506e2753894556470c39307ff13ccc05d89
shota0206 <sbhtgpnovel@gmail.com>
2017-08-26 15:54:15 +0900

    org

--- main.rb
+++ main.rb
@@ -1,1 +1,14 @@
 p "welcome to blackjack!"
+
+p "Start blackjack?(y/n)"
+
+input = gets
+
+puts input
+total = rand(1..13)
+puts total
+hit = gets
+if hit == "y" || hit == "yes"
+total = total + rand(1..13)
+end
+puts total
